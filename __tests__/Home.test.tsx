739c15d7d41cb15cb4eb12411ccb001a5997f536
gaia_galaxy <hitoshi.gaia.galaxy@gmail.com>
2022-01-01 22:31:55 +0900

    initial commit & initail test & install graphql codegen

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect'
+import Home from '../pages'
+
+it('should render title text', () => {
+  render(<Home />)
+  expect(screen.getByText('Next.js!')).toBeInTheDocument()
+})
